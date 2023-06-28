ce111e5547c7b1a5df75fa66d9f7b9f12d8ac6cc
James <jgc239@cornell.edu>
2023-06-28 20:50:47 +0000

    super annoying LLaMa vs Llama tokenizer bug in hf model

--- load_stable_vicuna_13b.sh
+++ load_stable_vicuna_13b.sh
@@ -8,6 +8,7 @@ git clone https://huggingface.co/decapoda-research/llama-13b-hf
 
 cd ~/iti_capstone/llama-13b-hf
 git lfs pull
+echo '{"bos_token": "", "eos_token": "", "model_max_length": 1000000000000000019884624838656, "tokenizer_class": "LlamaTokenizer", "unk_token": ""}' > ~/iti_capstone/llama-13b-hf/tokenizer_config.json
 cd ..
 
 git clone https://huggingface.co/CarperAI/stable-vicuna-13b-delta
