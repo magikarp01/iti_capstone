21b669b5c9c10d73372d479a417864ed9b5fdcfb
magikarp01 <philliphguo@gmail.com>
2023-08-04 15:33:11 -0400

    Reimplemented SmallModelActs as a child class of ModelActs

--- utils/new_probing_utils.py
+++ utils/new_probing_utils.py
@@ -43,6 +43,8 @@ from typing import TypeVar
 ModelActs = TypeVar("ModelActs")
 
 from dataclasses import dataclass
+import pickle
+
 
 @dataclass
 class ModelActs:
@@ -66,7 +68,7 @@ class ModelActs:
     probes = {}
     probe_accs = {}
 
-    labels: np.array = None
+    labels: np.ndarray or torch.T = None
     indices_trains: np.array = None
     indices_tests: np.array = None
 
@@ -120,14 +122,33 @@ class ModelActs:
         return clf, acc
 
 
-    def train_probes(self, act_type):
+    def train_probes(self, act_type, test_ratio=0.2, train_ratio=None, max_iter=1000):
         """
         Train probes on all provided activations of act_type in self.activations.
+        If train test split is not already set, set it with given keywords.
         """
+        if self.indices_tests is None and self.indices_trains is None:
+            self.set_train_test_split(self.activations[act_type][0].shape[0], test_ratio=test_ratio, train_ratio=train_ratio)
+        
+        if act_type not in self.probes:
+            self.probes[act_type] = {}
+            self.probe_accs[act_type] = {}
+
         for probe_index in self.activations[act_type]:
-            clf, acc = self._train_probe(act_type, probe_index)
+            clf, acc = self._train_probe(act_type, probe_index, max_iter=max_iter)
             self.probes[act_type][probe_index] = clf
             self.probe_accs[act_type][probe_index] = acc
+
+
+    def save_probes(self, id, filepath = "activations/"):
+        """
+        Save probes and probe accuracies to activations folder. Must be called after train_probes.
+        """
+        with open(f'{filepath}{id}_probes.pickle', 'wb') as handle:
+            pickle.dump(self.probes, handle)
+
+        with open(f'{filepath}{id}_probes_accs.pickle', 'wb') as handle:
+            pickle.dump(self.probe_accs, handle)
 
 
     def show_top_z_probes(self, topk=50):
@@ -148,7 +169,7 @@ class ModelActs:
                     print(f"{l}.{h}, ", end=" ")
 
 
-    def get_transfer_acc(self, act_type, probe_index, data_source: ModelActs):
+    def get_probe_transfer_acc(self, act_type, probe_index, data_source: ModelActs):
         """
         Get transfer accuracy of probes trained on these model acts on another set of model acts, on new data. 
         data_source is another ModelActs object that should already have been trained on probes for act_type (with its own train test split).
@@ -167,7 +188,9 @@ class ModelActs:
 
 class SmallModelActs(ModelActs):
     """
-    A class to handle small model activations ran on a user-defined dataset. Class has utilities to generate new activations based on a given model and dataset, and to store those activations for later use using TransformerLens (which is why model has to be small). Not memory efficient, but all-in-one for small models.
+    A class to handle small model activations ran on a user-defined dataset. Class has utilities to generate new activations based on a given model and dataset using TransformerLens (only compatible with 1 GPU, which is why model has to be small). 
+    
+    Generate and store all activations/probes for all heads at once. Not memory efficient, but all-in-one for small models.
 
     Initialize ModelActs class specifying which activation types to store.
     First, generate acts using gen_acts. Acts can be saved/loaded. Then, train probes on these acts using train_probes: these probes are stored in self.probes dictionary and accuracies are stored in self.probe_accs dictionary.
@@ -177,12 +200,13 @@ class SmallModelActs(ModelActs):
         dataset must have sample(sample_size) method returning indices of samples, sample_prompts, and sample_labels.
         act_types is a list of which activations to cache and operate on.
         """
+        super().__init__()
         self.model = model
         # self.model.cfg.total_heads = self.model.cfg.n_heads * self.model.cfg.n_layers
         self.dataset = dataset
         
-        self.attn_head_acts = None
-        self.indices = None
+        # indices of data
+        self.data_indices = None
         
         self.act_types=act_types
     
@@ -190,77 +214,127 @@ class SmallModelActs(ModelActs):
             np.random.seed(seed)
             torch.manual_seed(seed)
 
+    def _tensor_to_dict(self, tensor, head_tensor=False):
+        """
+        Helper method to convert a tensor in shape (component_pos, ...) into a dict of (component_pos) -> entry. 
+
+        If head_tensor is True, instead converts a tensor in shape (n_l, n_h, ...) into dict of (n_l, n_h) -> entry.
+        """
+        assert len(tensor.shape) >= 1
+        if head_tensor:
+            assert len(tensor.shape) >= 2
+        
+        entry_dict = {}
+        
+        if head_tensor:
+            for i in range(tensor.shape[0]):
+                for j in range(tensor.shape[1]):
+                    entry_dict[(i, j)] = tensor[i, j]
+        else:
+            for i in range(tensor.shape[0]):
+                entry_dict[i] = tensor[i]
+
+        return entry_dict
 
     def gen_acts(self, N = 1000, store_acts = True, filepath = "activations/", id = None, indices=None, storage_device="cpu", verbose=False):
         """
         Automatically generates activations over N samples (returned in self.indices). If store_acts is True, then store in activations folder. Indices are indices of samples in dataset.
         
-        
+        Args:
+            N: number of samples to generate activations over.
+            store_acts: whether to store activations in activations folder.
+            filepath: where to store activations.
+            id: id of activations, name used for storing activations.
+            indices: indices of samples of dataset to generate activations over.
+            storage_device: device to store activations on.
         """        
         if indices is None:
-            indices, all_prompts, all_labels = self.dataset.sample(N)
-        
+            data_indices, all_prompts, all_labels = self.dataset.sample(N)
+        else:
+            data_indices = indices
+
         cached_acts = defaultdict(list)
 
         # names filter for efficiency, only cache in self.act_types
         names_filter = lambda name: any([name.endswith(act_type) for act_type in self.act_types])
 
-        for i in tqdm(indices):
-            original_logits, cache = self.model.run_with_cache(self.dataset.all_prompts[i].to(self.model.cfg.device), names_filter=names_filter) # only cache z
+        for i in tqdm(data_indices):
+            original_logits, cache = self.model.run_with_cache(self.dataset.all_prompts[i].to(self.model.cfg.device), names_filter=names_filter) # only cache desired act types
             
             # store every act type in self.act_types
             for act_type in self.act_types:
 
                 if act_type == "result":
                     # get last seq position
-                    stored_acts = cache.stack_head_results(layer=-1, pos_slice=-1).squeeze().to(device=storage_device)
+                    stack_acts = cache.stack_head_results(layer=-1, pos_slice=-1).squeeze().to(device=storage_device)
+                    stack_acts = einops.rearrange(stack_acts, "(n_l n_h) d -> n_l n_h d", n_l = self.model.cfg.n_layers)
                 
                 elif act_type == "logits":
-                    stored_acts = original_logits[:,-1].to(device=storage_device) # logits of last token
-
-                elif act_type == "attn_scores":
-                    # shape of cache.stack_activation(act_type, layer = -1) is (n_l, 1, n_h, s, s)
-                    stored_acts = cache.stack_activation(act_type, layer=-1).squeeze().to(device=storage_device)
+                    stack_acts = original_logits[:,-1].squeeze().to(device=storage_device) # logits of last token, shape (vocab_size,)
 
                 else:
-                    stored_acts = cache.stack_activation(act_type, layer = -1)[:,0,-1].squeeze().to(device=storage_device)
-                    if verbose:
-                        print(f"{act_type=}, {cache.stack_activation(act_type, layer = -1).shape=}")
-                cached_acts[act_type].append(stored_acts)
-        
-        # convert lists of tensors into tensors
-        stored_acts = {}
+                    stack_acts = cache.stack_activation(act_type, layer = -1).to(device=storage_device)
+
+                    if act_type == "attn_scores":
+                        # shape of cache.stack_activation(act_type, layer = -1) is (n_l, 1, n_h, s, s)
+                        stack_acts = stack_acts.squeeze().to(device=storage_device)
+
+                    else:
+                        # z or mlp or resid 
+                        # shape (n_l, 1, seq_len, n_h, d_h) or (n_l, 1, seq_len, d_m)
+                        stack_acts = stack_acts[:,0,-1].squeeze()
+
+                if verbose:
+                    print(f"{act_type=}, {stack_acts.shape=}")
+
+                cached_acts[act_type].append(stack_acts)
+
         for act_type in self.act_types:
             try:
-                stored_acts[act_type] = torch.stack(cached_acts[act_type])
-            except: # if cached_acts cannot be stacked, if they are different sizes, probably for attention scores
-                stored_acts[act_type] = cached_acts[act_type]
+                stacked_acts = torch.stack(cached_acts[act_type], dim=0) # shape (n_acts, n_l, n_h, d)
+                stacked_acts = einops.rearrange(stacked_acts, "n_acts ... d -> ... n_acts d")
+            except:
+                print(f"Cannot stack activations, {act_type} not implemented yet")
+                stacked_acts = cached_acts[act_type]   
+                continue
 
-        self.stored_acts = stored_acts
-        self.indices = indices
+            if act_type == "result" or act_type == "z":
+                act_dict = self._tensor_to_dict(stacked_acts, head_tensor=True)
+            elif act_type == "logits":
+                act_dict = {0: stacked_acts}
+            elif act_type == "attn_scores":
+                print("attn_scores not implemented yet")
+                continue
+            else:
+                # mlp or resid
+                assert len(stacked_acts.shape) == 3
+                act_dict = self._tensor_to_dict(stacked_acts, head_tensor=False)
+
+            self.activations[act_type] = act_dict
         
+        self.data_indices = data_indices
         if store_acts:
             if id is None:
                 id = np.random.randint(10000)
-            torch.save(self.indices, f'{filepath}{id}_indices.pt')
+            torch.save(self.data_indices, f'{filepath}{id}_data_indices.pt')
             for act_type in self.act_types:
-                torch.save(self.stored_acts[act_type], f'{filepath}{id}_{act_type}_acts.pt')
+                with open(f'{filepath}{id}_{act_type}_acts.pt', 'wb') as f:
+                    pickle.dump(self.activations[act_type], f)
             print(f"Stored at {id}")
 
-        # return self.indices, self.attn_head_acts
 
     def load_acts(self, id, filepath = "activations/", load_probes=False):
         """
         Loads activations from activations folder. If id is None, then load the most recent activations. 
         If load_probes is True, load from saved probes.picle and all_heads_acc_np.npy files as well.
         """
-        indices = torch.load(f'{filepath}{id}_indices.pt')
+        data_indices = torch.load(f'{filepath}{id}_data_indices.pt')
 
-        self.stored_acts = {}
         for act_type in self.act_types:
-            self.stored_acts[act_type] = torch.load(f'{filepath}{id}_{act_type}_acts.pt')
+            with open(f'{filepath}{id}_{act_type}_acts.pt', 'rb') as f:
+                self.activations[act_type] = pickle.load(f)
         
-        self.indices = indices
+        self.data_indices = data_indices
 
         if load_probes:
             print("load_probes deprecated for now, please change to False")
@@ -268,150 +342,19 @@ class SmallModelActs(ModelActs):
                 self.probes = pickle.load(handle)
             self.all_head_accs_np = np.load(f'{filepath}{id}_all_head_accs_np.npy')
 
-        else:
-            self.probes = {}
-            # self.all_head_accs_np = None
-            self.probe_accs = {}
-
-        # return indices, attn_head_acts
 
     def control_for_iti(self, cache_interventions):
         """
-        Deprecated for now.
-        Subtracts the actual iti intervention vectors from the cached activations: this removes the direct
-        effect of ITI and helps us see the downstream effects.
+        Subtracts the actual iti intervention vectors from the cached activations: this removes the direct effect of ITI and helps us see the downstream effects.
+        
+        Args:
+            cache_interventions: tensor of shape (n_l, n_h, d_h) or (n_l, d_m)
         """
         # self.stored_acts["z"] -= einops.rearrange(cache_interventions, "n_l n_h d_h -> (n_l n_h) d_h")
-        self.stored_acts["z"] -= cache_interventions
+        intervention_dict = self._tensor_to_dict(cache_interventions, head_tensor=True)
 
-    def get_train_test_split(self, X_acts, test_ratio = 0.2, N = None):
-        """
-        Given X_acts, a Pytorch tensor of shape (num_samples, num_probes, d_probe), and test ratio, split acts and labels into train and test sets.
-        """
-        X_acts_list = [X_acts[i] for i in range(X_acts.shape[0])]
-        
-        indices = self.indices
-        
-        if N is not None:
-            X_acts_list = X_acts_list[:N]
-            indices = indices[:N]
-        
-        # print(self.attn_head_acts.shape)
-        # print(len(self.dataset.all_labels))
-        # print(len(indices))
-        # print(np.array(self.dataset.all_labels)[indices])
-        # print(len(np.array(self.dataset.all_labels)[indices]))
-
-        X_train, X_test, y_train, y_test, indices_train, indices_test = train_test_split(X_acts_list, np.array(self.dataset.all_labels)[indices], indices, test_size=test_ratio)
-        
-        X_train = torch.stack(X_train, axis = 0)
-        X_test = torch.stack(X_test, axis = 0)
-        
-        y_train = torch.from_numpy(np.array(y_train, dtype = np.float32))
-        y_test = torch.from_numpy(np.array(y_test, dtype = np.float32))
-        y_train = repeat(y_train, 'b -> b num_probes', num_probes=X_acts.shape[1])
-        y_test = repeat(y_test, 'b -> b num_probes', num_probes=X_acts.shape[1])
-
-        return X_train, X_test, y_train, y_test, indices_train, indices_test
-
-
-    def _train_probes(self, num_probes, X_train, X_test, y_train, y_test, max_iter=1000):
-        """
-        Helper function that all train_x_probes will call to train num_probes probes, after formatting X and y properly.
-        Flatten X so that X_train.shape = (num_examples, num_probes, d_probe)
-        y_train.shape = (num_examples, num_probes)
-        Returns list of probes and np array of probe accuracies
-        """
-        all_head_accs = []
-        probes = []
-        
-        for i in tqdm(range(num_probes)):
-            X_train_head = X_train[:,i,:]
-            X_test_head = X_test[:,i,:]
-
-            clf = LogisticRegression(max_iter=max_iter).fit(X_train_head.detach().numpy(), y_train[:, 0].detach().numpy()) # for every probe i, y_train[:, i] is the same
-            y_pred = clf.predict(X_train_head)
-            
-            y_val_pred = clf.predict(X_test_head.detach().numpy())
-            all_head_accs.append(accuracy_score(y_test[:, 0].numpy(), y_val_pred))
-            
-            probes.append(clf)
-
-        return probes, np.array(all_head_accs)
-
-
-    def train_probes(self, act_type, max_iter=1000):
-        """
-        Train arbitrary probes on any act type's activations (must've been included in self.act_types).
-        self.stored_acts[act_type] should be shape (num_examples, ..., d_probe), will be flattened in the middle
-        """
-        assert act_type in self.act_types
-        formatted_acts = torch.flatten(self.stored_acts[act_type], start_dim=1, end_dim=-2)
-
-        assert len(formatted_acts.shape) == 3
-
-        X_train, X_test, y_train, y_test, indices_train, indices_test = self.get_train_test_split(formatted_acts)
-        print(f"{X_train.shape}, {X_test.shape}, {y_train.shape}, {y_test.shape}")
-
-        probes, probe_accs = self._train_probes(formatted_acts.shape[1], X_train, X_test, y_train, y_test, max_iter=max_iter)
-
-        self.X_tests[act_type] = X_test
-        self.y_tests[act_type] = y_test
-        self.indices_trains[act_type] = indices_train
-        self.indices_tests[act_type] = indices_test
-
-        self.probes[act_type] = probes
-        self.probe_accs[act_type] = probe_accs
-
-
-    def save_probes(self, id, filepath = "activations/"):
-        """
-        Don't use, out of date since all_head_accs_np doesn't exist anymore.
-        Save probes and probe accuracies to activations folder. Must be called after train_probes.
-        """
-        with open(f'{filepath}{id}_probes.pickle', 'wb') as handle:
-            pickle.dump(self.probes, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-        np.save(f'{filepath}{id}_all_head_accs_np.npy', self.all_head_accs_np)
-
-    
-    def get_transfer_acc(self, act_type, data_source: ModelActs):
-        """
-        Get transfer accuracy of probes trained on this dataset on another dataset. 
-        data_source is another ModelActs object that should already have been trained on probes.
-        """
-        # data_labels = np.array(data_source.dataset.all_labels)[data_source.indices]
-        data_labels = data_source.y_tests[act_type][:,0].numpy()
-
-        accs = []
-
-        for i, clf in tqdm(enumerate(self.probes[act_type])):
-            # acts = data_source.attn_head_acts[:, i, :]
-            acts = data_source.X_tests[act_type][:, i, :]
-            y_pred = clf.predict(acts)
-            
-            accs.append(accuracy_score(data_labels, y_pred))
-        
-        return np.array(accs)
-
-    def show_top_z_probes(self, topk=50):
-        """
-        Utility to print the most accurate heads. Out of date with probe_generalization merge.
-        """
-        probe_accuracies = torch.tensor(einops.rearrange(self.probe_accs["z"], "(n_l n_h) -> n_l n_h", n_l=self.model.cfg.n_layers))
-        top_head_indices = torch.topk(einops.rearrange(probe_accuracies, "n_l n_h -> (n_l n_h)"), k=topk).indices # take top k indices
-
-        top_probe_heads = torch.zeros(size=(self.model.cfg.total_heads,))
-        top_probe_heads[top_head_indices] = 1
-        top_probe_heads = einops.rearrange(top_probe_heads, "(n_l n_h) -> n_l n_h", n_l=self.model.cfg.n_layers)
-        for l in range(self.model.cfg.n_layers):
-            for h in range(self.model.cfg.n_heads):
-                if top_probe_heads[l, h] == 1:
-                    print(f"{l}.{h}, ", end=" ")
-
-
-
-
+        for probe_index in self.activations["z"]:
+            self.activations["z"][probe_index] -= intervention_dict[probe_index]
 
 
 class ModelActsLargeSimple(ModelActs):
